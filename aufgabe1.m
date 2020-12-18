dcbeee8e4c21b0bd6bccda798074b419a1c6bbcb
Phil Dörich <doerich.phil@gmail.com>
2020-12-18 13:08:52 +0100

    implizite Lösung und Plotversuche

--- aufgabe1.m
+++ aufgabe1.m
@@ -1,6 +1,6 @@
 %Variablen
 L       = 1;
-T       = 6;   %Betrachtungszeit
+T       = 0.01;   %Betrachtungszeit
 n       = 100;
 K       = 1;
 tMax    = 100;
@@ -31,16 +31,23 @@ funI    = @(x) 3*sin(pi*x) + 5*sin(4*pi*x);
 funII   = @(x) 1;
 funIII  = @(x) x;
 
+%Anfangswerte
+for j = 1:xMax
+    uI(j, 1)    = funI(x(j));
+    uII(j, 1)   = funII(x(j));
+    uIII(j, 1)  = funIII(x(j));
+end
+
 %Bausteine berechnen
 for i = 1:n
-    B_nI(i, 1)      = (2/L) .* integral(@(x) funI(x).*funSin(i, x),0,L);
-    B_nII(i, 1)     = (2/L) .* integral(@(x) funII(x).*funSin(i, x),0,L);
-    B_nIII(i, 1)    = (2/L) .* integral(@(x) funIII(x).*funSin(i, x),0,L);
+    B_nI(i, 1)      = (2/L) * integral(@(x) funI(x).*funSin(i, x),0,L);
+    B_nII(i, 1)     = (2/L) * integral(@(x) funII(x).*funSin(i, x),0,L);
+    B_nIII(i, 1)    = (2/L) * integral(@(x) funIII(x).*funSin(i, x),0,L);
 end
 
 %Exakte Lösung
-for j = 1:xMax
-    for k = 1:tMax
+for j = 2:xMax-1
+    for k = 2:tMax-1
         tempI   = 0;
         tempII  = 0;
         tempIII = 0;
@@ -71,19 +78,9 @@ for j = 2:xMax-1
     end
 end
 
-%Numerische Lösung (implizit)
-%for j = 2:xMax-1
-    %for k = 2:tMax-1
-    %    wI(j,k)     = -d*wI(j-1, k+1) + (1 + 2*d)*wI(j, k+1) - d*wI(j+1, k+1);
-   %     wII(j,k)    = -d*wII(j-1, k+1) + (1 + 2*d)*wII(j, k+1) - d*wII(j+1, k+1);
-  %      wIII(j,k)   = -d*wIII(j-1, k+1) + (1 + 2*d)*wIII(j, k+1) - d*wIII(j+1, k+1);
- %   end
-%end
-
-
-
-vec1    = zeros(xMax, 1);
-vec2    = zeros(xMax-1, 1);
+%Tridiagonalmatrix A
+vec1    = zeros(xMax-2, 1);
+vec2    = zeros(xMax-3, 1);
 
 for j = 1:xMax
     vec1(j, 1) = 1+2*d;
@@ -91,29 +88,77 @@ end
 for j = 1:xMax-1
     vec2(j, 1) = -d;
 end
-
 A1 = diag(vec1);
 A2 = diag(vec2, 1);
 A3 = diag(vec2, -1);
+A = A1 + A2 + A3;
 
-%A = A1 + A2 + A3;
+RI = zeros(xMax, 1);
+RII = zeros(xMax, 1);
+RIII = zeros(xMax, 1);
+for i = 1:xMax-1
+  RI(i, 1) = funI(x(i+1));
+  RII(i, 1) = funII(x(i+1));
+  RIII(i, 1) = funIII(x(i+1));
+end
+
+for i = 1:xMax
+    xVecI = A\RI(:,i); % erster x-Wert, wird neuer r-Vektor
+    RI = [RI, xVecI];
+    xVecII = A\RII(:,i); % erster x-Wert, wird neuer r-Vektor
+    RII = [RII, xVecII];
+    xVecIII = A\RIII(:,i); % erster x-Wert, wird neuer r-Vektor
+    RIII = [RIII, xVecIII];
+end 
 
 
+% figure('Name', 'Plot i)','NumberTitle','off')
+% plot(x, uI(1:xMax, 1));
+% 
+% figure('Name', 'Plot ii)','NumberTitle','off')
+% plot(x, uII(1:xMax, 1));
+% 
+% figure('Name', 'Plot iii)','NumberTitle','off')
+% plot(x, uIII(1:xMax, 1));
+% 
+% figure('Name', 'Plot numerical (explizit) i)','NumberTitle','off')
+% plot(x, vI);
+% 
+% figure('Name', 'Plot numerical (explizit) ii)','NumberTitle','off')
+% plot(x, vII);
+% 
+% figure('Name', 'Plot numerical (explizit) iii)','NumberTitle','off')
+% plot(x, vIII);
+% 
+% figure('Name', 'Plot numerical (implizit) i)','NumberTitle','off')
+% plot(x, RI(1:xMax, 1));
+% 
+% figure('Name', 'Plot numerical (implizit) ii)','NumberTitle','off')
+% plot(x, RII(1:xMax, 1));
+% 
+% figure('Name', 'Plot numerical (implizit) iii)','NumberTitle','off')
+% plot(x, RIII(1:xMax, 1));
 
-figure('Name', 'Plot i)','NumberTitle','off')
-plot(x, uI(1:xMax, 1));
 
-figure('Name', 'Plot ii)','NumberTitle','off')
-plot(x, uII(1:xMax, 1));
+% figure('Name', 'exakt und explizite numerische Lösung (i)','NumberTitle','off')
+% plot(x, uI(1:xMax,1), x, uI(1:xMax,  ceil(tMax/6)),'r', x, uI(1:xMax, 2* ceil(tMax/6)), 'm', x, uI(1:xMax, 3* ceil(tMax/6)), 'b', x, uI(1:xMax, 4* ceil(tMax/6)), 'g', x, uI(1:xMax, 5* ceil(tMax/6)), 'y');
+% % hold on
+% 
+% figure('Name', 'test(i)','NumberTitle','off')
+% hold on 
+% plot(x, vI(1:xMax,1))
+% plot(x, vI(1:xMax,  ceil(tMax/6)),'r')
+% plot(x, vI(1:xMax, 2* ceil(tMax/6)), 'm')
+% plot(x, vI(1:xMax, 3* ceil(tMax/6)), 'b')
+% plot(x, vI(1:xMax, 4* ceil(tMax/6)), 'g')
+% plot(x, vI(1:xMax, 5* ceil(tMax/6)), 'y')
+% hold off
 
-figure('Name', 'Plot iii)','NumberTitle','off')
-plot(x, uIII(1:xMax, 1));
+figure('Name', 'exakt und explizite numerische Lösung (i)','NumberTitle','off')
+plot(x, uI(1:xMax,1))
 
-figure('Name', 'Plot numerical i)','NumberTitle','off')
-plot(x, vI);
-
-figure('Name', 'Plot numerical ii)','NumberTitle','off')
-plot(x, vII);
-
-figure('Name', 'Plot numerical iii)','NumberTitle','off')
-plot(x, vIII);
+hold on 
+plot(x, uI(1:xMax,  ceil(tMax/6)),'r')
+plot(x, vI(1:xMax,1),'+')
+plot(x, vI(1:xMax,  ceil(tMax/6)),'o')
+hold off
